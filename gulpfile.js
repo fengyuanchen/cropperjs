2cee12105e7684702fdaa92f498e9b9d6893d60f
Fengyuan Chen <cfy.org@gmail.com>
2015-12-15 14:12:20 +0800

    Add csscomb task

--- gulpfile.js
+++ gulpfile.js
@@ -129,17 +129,18 @@ gulp.task('css', ['csslint'], function () {
   return gulp.src(styles.main)
     .pipe(plugins.replace(replacement.regexp, replacement.filter))
     .pipe(plugins.autoprefixer({
-        browsers: [
-          'Android 2.3',
-          'Android >= 4',
-          'Chrome >= 20',
-          'Firefox >= 24',
-          'Explorer >= 8',
-          'iOS >= 6',
-          'Opera >= 12',
-          'Safari >= 6'
-        ]
-      }))
+      browsers: [
+        'Android 2.3',
+        'Android >= 4',
+        'Chrome >= 20',
+        'Firefox >= 24',
+        'Explorer >= 8',
+        'iOS >= 6',
+        'Opera >= 12',
+        'Safari >= 6'
+      ]
+    }))
+    .pipe(plugins.csscomb())
     .pipe(gulp.dest(styles.dest))
     .pipe(gulp.dest(styles.site))
     .pipe(plugins.rename(styles.min))
