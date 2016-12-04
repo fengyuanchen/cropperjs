cbd74f7c114f49b8ee19b9deee1462dab78f2e4c
Chen Fengyuan <chenfengyuan@outlook.com>
2016-12-04 21:36:12 +0800

    Added rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+const babel = require('rollup-plugin-babel');
+
+module.exports = {
+  entry: 'src/js/cropper.js',
+  targets: [
+    {
+      dest: 'dist/cropper.js',
+    },
+    {
+      dest: 'docs/js/cropper.js',
+    },
+  ],
+  format: 'umd',
+  moduleName: 'Cropper',
+  plugins: [
+    babel({
+      exclude: '/node_modules/**',
+    }),
+  ],
+  sourceMap: true,
+};
