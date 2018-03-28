34c24ea86cd222190282a9bda13124a6f23f19e3
Chen Fengyuan <chenfengyuan@outlook.com>
2018-03-28 20:20:38 +0800

    style: improve

--- src/js/methods.js
+++ src/js/methods.js
@@ -743,8 +743,6 @@ export default {
       dstHeight = srcHeight;
     }
 
-    // All the numerical parameters should be integer for `drawImage`
-    // https://github.com/fengyuanchen/cropper/issues/476
     const params = [
       srcX,
       srcY,
@@ -764,6 +762,8 @@ export default {
       );
     }
 
+    // All the numerical parameters should be integer for `drawImage`
+    // https://github.com/fengyuanchen/cropper/issues/476
     context.drawImage(source, ...params.map(param => Math.floor(normalizeDecimalNumber(param))));
 
     return canvas;
