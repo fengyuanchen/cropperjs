81c6d49988d5d8f95dc5737f7cc836b489014af5
Chen Fengyuan <chenfengyuan@outlook.com>
2022-01-30 15:24:51 +0800

    fix(cropperjs): add missing initial attribute

--- packages/cropperjs/src/template.ts
+++ packages/cropperjs/src/template.ts
@@ -4,7 +4,7 @@ export default (
     + '<cropper-shade hidden></cropper-shade>'
     + '<cropper-handle action="select" plain></cropper-handle>'
     + '<cropper-selection initial-coverage="0.5" movable resizable zoomable>'
-      + '<cropper-grid role="grid" covered></cropper-grid>'
+      + '<cropper-grid role="grid" bordered covered></cropper-grid>'
       + '<cropper-crosshair centered></cropper-crosshair>'
       + '<cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>'
       + '<cropper-handle action="n-resize"></cropper-handle>'
