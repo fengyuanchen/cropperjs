c7ee81cd9150bab28ea430f275ecc1490943bc7c
Chen Fengyuan <chenfengyuan@outlook.com>
2022-02-02 13:12:42 +0800

    build: simplify config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.js': 'eslint --fix',
-  '*.{css,scss,html}': 'stylelint --fix',
+  '*.{css,scss}': 'stylelint --fix',
 };
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  extends: 'stylelint-config-recommended-scss',
+  extends: 'stylelint-config-standard-scss',
   plugins: [
     'stylelint-order',
   ],
   rules: {
     'order/properties-alphabetical-order': true,
   },
-  overrides: [
-    {
-      files: ['**/*.html'],
-      customSyntax: 'postcss-html',
-    },
-  ],
 };
