4e09be600391ea574080f6217f4df32695a58915
Chen Fengyuan <chenfengyuan@outlook.com>
2024-05-26 15:31:35 +0800

    docs(element-selection): improve limiting boundaries example

--- docs/.vitepress/components/CropperSelectionExample.vue
+++ docs/.vitepress/components/CropperSelectionExample.vue
@@ -1,19 +1,74 @@
 <template>
   <div class="cropper-container">
-    <cropper-canvas ref="cropperCanvas" background>
-      <cropper-image :src="src" alt="Picture"></cropper-image>
-      <cropper-selection initial-coverage="0.5" movable resizable outlined @change="onCropperSelectionChange">
-        <cropper-grid role="grid" covered></cropper-grid>
-        <cropper-crosshair centered></cropper-crosshair>
-        <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)"></cropper-handle>
-        <cropper-handle action="n-resize"></cropper-handle>
-        <cropper-handle action="e-resize"></cropper-handle>
-        <cropper-handle action="s-resize"></cropper-handle>
-        <cropper-handle action="w-resize"></cropper-handle>
-        <cropper-handle action="ne-resize"></cropper-handle>
-        <cropper-handle action="nw-resize"></cropper-handle>
-        <cropper-handle action="se-resize"></cropper-handle>
-        <cropper-handle action="sw-resize"></cropper-handle>
+    <form>
+      <fieldset>
+        <legend>Within:</legend>
+        <input
+          id="inputWithinCanvas"
+          v-model="within"
+          type="radio"
+          name="within"
+          value="canvas"
+        >
+        <label for="inputWithinCanvas">canvas</label>
+        <input
+          id="inputWithinImage"
+          v-model="within"
+          type="radio"
+          name="within"
+          value="image"
+        >
+        <label for="inputWithinImage">image</label>
+        <input
+          id="inputWithinNone"
+          v-model="within"
+          type="radio"
+          name="within"
+          value="none"
+        >
+        <label for="inputWithinNone">none</label>
+      </fieldset>
+    </form>
+    <cropper-canvas
+      ref="cropperCanvas"
+      :key="within"
+      background
+    >
+      <cropper-image
+        ref="cropperImage"
+        :src="src"
+        alt="Picture"
+        @transform="onCropperImageTransform"
+      />
+      <cropper-handle
+        action="move"
+        plain
+      />
+      <cropper-selection
+        ref="cropperSelection"
+        initial-coverage="0.5"
+        movable
+        resizable
+        outlined
+        @change="onCropperSelectionChange"
+      >
+        <cropper-grid
+          role="grid"
+          covered
+        />
+        <cropper-crosshair centered />
+        <cropper-handle
+          action="move"
+          theme-color="rgba(255, 255, 255, 0.35)"
+        />
+        <cropper-handle action="n-resize" />
+        <cropper-handle action="e-resize" />
+        <cropper-handle action="s-resize" />
+        <cropper-handle action="w-resize" />
+        <cropper-handle action="ne-resize" />
+        <cropper-handle action="nw-resize" />
+        <cropper-handle action="se-resize" />
+        <cropper-handle action="sw-resize" />
       </cropper-selection>
     </cropper-canvas>
   </div>
@@ -21,6 +76,9 @@
 
 <script lang="ts">
 import type CropperCanvas from '@cropper/element-canvas';
+import type CropperImage from '@cropper/element-image';
+import type CropperSelection from '@cropper/element-selection';
+import type { Selection } from '@cropper/element-selection';
 
 const { BASE_URL } = import.meta.env;
 
@@ -29,20 +87,99 @@ export default {
   data() {
     return {
       src: `${BASE_URL}picture.jpg`,
+      within: 'canvas',
     };
   },
   methods: {
-    onCropperSelectionChange(event: CustomEvent) {
-      const cropperCanvas = this.$refs.cropperCanvas as CropperCanvas;
-      const selection = event.detail;
+    inSelection(selection: Selection, maxSelection: Selection) {
+      return (
+        selection.x >= maxSelection.x
+        && selection.y >= maxSelection.y
+        && (selection.x + selection.width) <= (maxSelection.x + maxSelection.width)
+        && (selection.y + selection.height) <= (maxSelection.y + maxSelection.height)
+      );
+    },
+    onCropperImageTransform(event: CustomEvent) {
+      if (this.within !== 'image') {
+        return;
+      }
 
-      if (cropperCanvas && (
-        selection.x < 0
-        || selection.y < 0
-        || (selection.x + selection.width) > cropperCanvas.offsetWidth
-        || (selection.y + selection.height) > cropperCanvas.offsetHeight
-      )) {
+      const cropperCanvas = this.$refs.cropperCanvas as CropperCanvas;
+      const cropperImage = this.$refs.cropperImage as CropperImage;
+      const cropperSelection = this.$refs.cropperSelection as CropperSelection;
+      const cropperCanvasRect = cropperCanvas.getBoundingClientRect();
+
+      // 1. Clone the cropper image.
+      const cropperImageClone = cropperImage.cloneNode() as CropperImage;
+
+      // 2. Apply the new matrix to the cropper image clone.
+      cropperImageClone.style.transform = `matrix(${event.detail.matrix.join(', ')})`;
+
+      // 3. Make the cropper image clone invisible.
+      cropperImageClone.style.opacity = '0';
+
+      // 4. Append the cropper image clone to the cropper canvas.
+      cropperCanvas.appendChild(cropperImageClone);
+
+      // 5. Compute the boundaries of the cropper image clone.
+      const cropperImageRect = cropperImageClone.getBoundingClientRect();
+
+      // 6. Remove the cropper image clone.
+      cropperCanvas.removeChild(cropperImageClone);
+
+      const selection = cropperSelection as Selection;
+      const maxSelection: Selection = {
+        x: cropperImageRect.left - cropperCanvasRect.left,
+        y: cropperImageRect.top - cropperCanvasRect.top,
+        width: cropperImageRect.width,
+        height: cropperImageRect.height,
+      };
+
+      if (!this.inSelection(selection, maxSelection)) {
         event.preventDefault();
+      }
+    },
+    onCropperSelectionChange(event: CustomEvent) {
+      if (this.within === 'none') {
+        return;
+      }
+
+      const cropperCanvas = this.$refs.cropperCanvas as CropperCanvas;
+      const cropperCanvasRect = cropperCanvas.getBoundingClientRect();
+      const selection = event.detail as Selection;
+
+      switch (this.within) {
+        case 'canvas': {
+          const maxSelection: Selection = {
+            x: 0,
+            y: 0,
+            width: cropperCanvasRect.width,
+            height: cropperCanvasRect.height,
+          };
+
+          if (!this.inSelection(selection, maxSelection)) {
+            event.preventDefault();
+          }
+          break;
+        }
+
+        case 'image': {
+          const cropperImage = this.$refs.cropperImage as CropperImage;
+          const cropperImageRect = cropperImage.getBoundingClientRect();
+          const maxSelection: Selection = {
+            x: cropperImageRect.left - cropperCanvasRect.left,
+            y: cropperImageRect.top - cropperCanvasRect.top,
+            width: cropperImageRect.width,
+            height: cropperImageRect.height,
+          };
+
+          if (!this.inSelection(selection, maxSelection)) {
+            event.preventDefault();
+          }
+          break;
+        }
+
+        default:
       }
     },
   },
@@ -57,8 +194,25 @@ export default {
   margin-top: 1rem;
   padding: 1.25rem 1.5rem;
 
-  :deep(cropper-canvas) {
-    height: 360px;
+  fieldset {
+    border: 1px solid var(--vp-c-divider);
+    border-radius: 0.375rem;
+    margin-bottom: 1rem;
+    padding: 0.25rem 0.75rem 0.75rem 0.75rem;
+
+    > input {
+      margin: 0 0.25rem 0 0;
+      transform: translateY(-0.5px);
+      vertical-align: middle;
+    }
+
+    > label {
+      margin-right: 0.5rem;
+    }
+  }
+
+  cropper-canvas {
+    height: 320px;
   }
 }
 </style>
