fc3366a781da65fe91ec5aeda724fd97a4c84b79
Chen Fengyuan <chenfengyuan@outlook.com>
2024-06-28 20:41:40 +0800

    docs: unregister service worker

---
+++ v2/service-worker.js
@@ -0,0 +1,13 @@
+if ('serviceWorker' in navigator) {
+  self.addEventListener('install', () => {
+    self.skipWaiting();
+  });
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => navigator.serviceWorker.getRegistrations())
+      .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+    );
+  });
+}
