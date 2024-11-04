b67a11bf764bc1ae2165a295610641b1eac40220
Hans Baker <hansbaker90@gmail.com>
2024-11-04 03:45:50 -0800

    Fix regex expressions - anchors not being respected (#1211)
    Regexs like `/^a|b|c$/` match `a` at start of text, `c` at end of text, or `b`.  I assume this does not match the author's original intent

--- src/js/constants.js
+++ src/js/constants.js
@@ -57,10 +57,10 @@ export const EVENT_ZOOM = 'zoom';
 export const MIME_TYPE_JPEG = 'image/jpeg';
 
 // RegExps
-export const REGEXP_ACTIONS = /^e|w|s|n|se|sw|ne|nw|all|crop|move|zoom$/;
+export const REGEXP_ACTIONS = /^(e|w|s|n|se|sw|ne|nw|all|crop|move|zoom)$/;
 export const REGEXP_DATA_URL = /^data:/;
 export const REGEXP_DATA_URL_JPEG = /^data:image\/jpeg;base64,/;
-export const REGEXP_TAG_NAME = /^img|canvas$/i;
+export const REGEXP_TAG_NAME = /^(img|canvas)$/i;
 
 // Misc
 // Inspired by the default width and height of a canvas element.
